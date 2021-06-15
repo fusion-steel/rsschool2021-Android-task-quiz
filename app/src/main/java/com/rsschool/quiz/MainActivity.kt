2f5cec2f9187d2be843b827003bc417e4e520a3e
fusion-steel <pavelgently@gmail.com>
2021-06-16 00:01:18 +0300

    Feat: add createFragment and launchNextFragment methods.

--- app/src/main/java/com/rsschool/quiz/MainActivity.kt
+++ app/src/main/java/com/rsschool/quiz/MainActivity.kt
@@ -2,10 +2,34 @@ package com.rsschool.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rsschool.quiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+            .also { setContentView(it.root) }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(binding.mainContainer.id, createFragment())
+                .commit()
+        }
+    }
+
+    private fun createFragment(): FragmentQuiz {
+        return FragmentQuiz.newInstance()
+    }
+
+    private fun launchNextFragment() {
+        supportFragmentManager
+            .beginTransaction()
+            .addToBackStack(null)
+            .replace(binding.mainContainer.id, createFragment())
+            .commit()
     }
 }
