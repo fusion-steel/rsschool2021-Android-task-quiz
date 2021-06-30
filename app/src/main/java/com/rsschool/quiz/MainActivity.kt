f44f2dfe50d083011e1dce0e298974b68fc9fe34
fusion-steel <pavelgently@gmail.com>
2021-06-30 23:14:39 +0300

    feat: implement main activity

--- app/src/main/java/com/rsschool/quiz/MainActivity.kt
+++ app/src/main/java/com/rsschool/quiz/MainActivity.kt
@@ -4,32 +4,84 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rsschool.quiz.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Navigator {
 
     private lateinit var binding: ActivityMainBinding
+
+    private var current = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-            .also { setContentView(it.root) }
+        setContentView(binding.root)
 
         if (savedInstanceState == null) {
+            startQuiz()
+        }
+
+    }
+
+    override fun startQuiz() {
+        current = 0
+        changeTheme()
+        UserAnswers.answersID.clear()
+        val fragment = FragmentQuiz.newInstance(current)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.mainContainer.id, fragment)
+            .commit()
+    }
+
+    override fun launchNext() {
+        if (current == Content.questions.size - 1) {
+            launchResult()
+        } else {
+            current++
+            changeTheme()
             supportFragmentManager
                 .beginTransaction()
-                .add(binding.mainContainer.id, createFragment())
+                .replace(binding.mainContainer.id, FragmentQuiz.newInstance(current))
+                .addToBackStack(null)
                 .commit()
         }
     }
 
-    private fun createFragment(): FragmentQuiz {
-        return FragmentQuiz.newInstance()
+    override fun launchPrevious() {
+        dropLastAnswer()
+        if (current > 0) {
+            current--
+            changeTheme()
+            supportFragmentManager.popBackStack()
+        } else {
+            finish()
+        }
     }
 
-    private fun launchNextFragment() {
+    override fun launchResult() {
         supportFragmentManager
             .beginTransaction()
-            .addToBackStack(null)
-            .replace(binding.mainContainer.id, createFragment())
+            .replace(binding.mainContainer.id, FragmentResult.newInstance())
             .commit()
+    }
+
+    private fun changeTheme() {
+        val currentTheme = when (current) {
+            0 -> R.style.Theme_Quiz_First
+                .also { window.statusBarColor = getColor(R.color.deep_orange_100_dark) }
+            1 -> R.style.Theme_Quiz_Second
+                .also { window.statusBarColor = getColor(R.color.yellow_100_dark) }
+            2 -> R.style.Theme_Quiz_Third
+                .also { window.statusBarColor = getColor(R.color.light_green_100_dark) }
+            3 -> R.style.Theme_Quiz_Fourth
+                .also { window.statusBarColor = getColor(R.color.cyan_100_dark) }
+            else -> R.style.Theme_Quiz_Fifth
+                .also { window.statusBarColor = getColor(R.color.deep_purple_100_dark) }
+        }
+        setTheme(currentTheme)
+    }
+
+    private fun dropLastAnswer() {
+//        println("remove ${ UserAnswers.answersID.last() }")
+        UserAnswers.answersID.removeAt(UserAnswers.answersID.lastIndex)
     }
 }
